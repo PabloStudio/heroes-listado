5722074cc469093ce531ba0562b20cdffee57e54
Pablo Studio <91836792+PabloStudio@users.noreply.github.com>
2022-03-13 19:23:16 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+// Importaciones.
 import { Component } from '@angular/core';
 
+// Decorador.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 
+// Clase.
 export class AppComponent 
 {
 
